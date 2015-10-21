05a9341b77b51fc3490302b248289af4261e4b9e
ProjectivePlane <derek.bell.ie@gmail.com>
2015-10-21 19:52:48 +0100

    Added print-collatz-sequence-lengths

--- collatz.fs
+++ collatz.fs
@@ -17,4 +17,12 @@
     dup 1 > if
 	collatz-func recurse 1+
     then ;
-    
+
+: print-collatz-sequence-lengths ( n --)
+    dup 1 > if
+	1 ?do
+	    i collatz-sequence-length i . ." " . cr
+	loop
+    else
+	." Error: argument must be greater than 1."
+    endif ;
